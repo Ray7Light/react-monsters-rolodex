6868ab0cc2369126e6a8230f750d50ce5166a224
Marcin <m.ficek@infomex.pl>
2020-01-13 10:27:12 +0100

    Updates

---
+++ precache-manifest.2b93d2e557b75bc23635462edb15f735.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9cd845f41f5ebed9956e4478346037d",
+    "url": "/react-monsters-rolodex/index.html"
+  },
+  {
+    "revision": "ac85d17e3d6e0162bf4c",
+    "url": "/react-monsters-rolodex/static/css/main.c28d3649.chunk.css"
+  },
+  {
+    "revision": "77115cfbcec72719740d",
+    "url": "/react-monsters-rolodex/static/js/2.7db90bec.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-monsters-rolodex/static/js/2.7db90bec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ac85d17e3d6e0162bf4c",
+    "url": "/react-monsters-rolodex/static/js/main.06c34fba.chunk.js"
+  },
+  {
+    "revision": "2a54011d4b12cab4d01e",
+    "url": "/react-monsters-rolodex/static/js/runtime-main.753a6df2.js"
+  }
+]);
